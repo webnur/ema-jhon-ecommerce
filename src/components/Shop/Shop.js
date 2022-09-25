4b2bd2368749f2649cdecd3939eb7b0f0451ead7
webnur <mdnuruli291@gmai.com>
2022-09-26 05:56:54 +0600

    ema jhon ecommerce basic done module 49

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css'
+const Shop = () => {
+
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
+    useEffect(() => {
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    },[])
+
+    const handleAddToCart = (product) => {
+        console.log(product)
+        const newCart = [...cart, product]
+        setCart(newCart)
+    }
+    return (
+        <div className='shop-container'>
+            <div className="product-container">
+                
+               
+                {
+                    products.map(product => <Product 
+                        product={product}
+                         key={product.id}
+                         handleAddToCart={handleAddToCart}
+                         ></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <h1>cart container</h1>
+                <p>Selected Items: {cart.length}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
